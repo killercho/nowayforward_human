81ca432f5ed1b33a51cb4cad6f07d69eb0b5a815
Syndamia <kamen@syndamia.com>
2025-01-18 20:59:00 +0200

    feat(controllers): Meta now doesn't call a function if it doesn't exist

--- controllers/meta.php
+++ controllers/meta.php
@@ -4,9 +4,15 @@ foreach (glob("../models/*.php") as $filename) {
     include $filename;
 }
 
+function call_handler(string $name) {
+    if (function_exists($name)) {
+        call_user_func($name);
+    }
+}
+
 match ($_SERVER['REQUEST_METHOD']) {
-    'POST' => on_post(),
-    'GET'  => on_get(),
-    'PUT'  => on_put(),
-    'DELETE' => on_delete(),
+    'POST' => call_handler('on_post'),
+    'GET'  => call_handler('on_get'),
+    'PUT'  => call_handler('on_put'),
+    'DELETE' => call_handler('on_delete'),
 };
