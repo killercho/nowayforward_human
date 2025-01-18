e9af7ba259281472e299ae06daee361b44cfdb42
Syndamia <kamen@syndamia.com>
2025-01-18 20:59:11 +0200

    feat(views): Rework sample_archive into the new format

---
+++ views/sample_archive/index.php
@@ -0,0 +1,11 @@
+<?php
+    include '../meta.php';
+    runController('archive_page');
+?>
+
+<form action="/sample_archive/index.php" method="POST">
+    <input type="text" name="page_url">
+    <input type="submit" name="archive_page_button">
+</form>
+
+<?php end_page(); ?>
