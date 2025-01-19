97ce22e66707e82fbd160edeb97de0432e2be767
Syndamia <kamen@syndamia.com>
2025-01-19 15:55:32 +0200

    feat(views): Implement initial UI design

--- views/header.php
+++ views/header.php
@@ -5,6 +5,7 @@
     <meta name="robots" content="index, follow">
     <meta name="theme-color" content="#2b2b2e">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
     <title><?php echo htmlspecialchars($title ?? "");?></title>
 </head>
 <body>
