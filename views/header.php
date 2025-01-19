877320142b415a5ee9b8e3eff75be1470c0bab46
Syndamia <kamen@syndamia.com>
2025-01-19 15:53:10 +0200

    feat(views): Add meta tags, PHP title logic and navbar

--- views/header.php
+++ views/header.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
-<html lang="bg">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Test</title>
+    <meta name="robots" content="index, follow">
+    <meta name="theme-color" content="#2b2b2e">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo htmlspecialchars($title ?? "");?></title>
 </head>
 <body>
+    <header>
+        <nav>
+            <a href="/test/index.php">Test</a>
+            <a href="/sample_archive/index.php">Sample Archive</a>
+        </nav>
+    </header>
+    <article>
