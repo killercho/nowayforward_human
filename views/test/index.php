c4da5629654f7cde95a91c51fc69c9fcbea9613d
Syndamia <kamen@syndamia.com>
2025-01-19 15:53:55 +0200

    feat(views): Implement more sophisticated printing of user info

--- views/test/index.php
+++ views/test/index.php
@@ -1,11 +1,20 @@
 <?php
     include '../meta.php';
-    print_r(Database\User::get_all());
 ?>
 
 <form action="/test1/index.php" method="POST">
     <input type="text" name="Username" placeholder="Username">
     <input type="submit">
 </form>
+
+<?php foreach(Database\User::get_all() as $user):?>
+    <section class="card">
+        <section class="quickinfo">
+            <?php print_r($user); ?>
+        </section>
+        <strong><?php echo $user->Username ?></strong>
+        <em><?php echo $user->Role ?></em>
+    </section>
+<?php endforeach;?>
 
 <?php end_page(); ?>
