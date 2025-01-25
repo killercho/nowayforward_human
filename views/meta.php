bd2b3acd9c4f54fc13d341b977371393be50fbd6
Syndamia <kamen@syndamia.com>
2025-01-25 19:59:06 +0200

    feat(views/meta): include_once instead of include

--- views/meta.php
+++ views/meta.php
@@ -1,16 +1,17 @@
 <?php
 
-include "../header.php";
+include_once "../header.php";
 
 function end_page() {
-    include "../footer.php";
+    include_once "../footer.php";
 }
 
+include_once "../../models/database.php";
 foreach (glob("../../models/*.php") as $filename) {
-    include $filename;
+    include_once $filename;
 }
 
 function runController(string $name) {
-    include "../../controllers/$name.php";
-    include '../../controllers/meta.php';
+    include_once "../../controllers/$name.php";
+    include_once '../../controllers/meta.php';
 }
