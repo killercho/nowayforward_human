decc7e47146584dcc02171793072462ce0f817fe
Syndamia <kamen@syndamia.com>
2025-01-25 19:58:27 +0200

    feat(models/cookies): Added model with proper logic for cookie creation

---
+++ models/cookies.php
@@ -0,0 +1,30 @@
+<?php
+namespace Database;
+use PDO;
+
+class Cookie extends Table {
+    public $UID;
+    public $Token;
+    public $Expires;
+
+    static function create(string $UID) : string {
+        $Token = uuidv4();
+        Table::_create(
+            "Cookies",
+            "(UID, Token, Expires)",
+            "(\"$UID\", \"$Token\", NOW() + INTERVAL 30 DAY)",
+        );
+        return $Token;
+    }
+}
+
+// Taken from https://stackoverflow.com/a/15875555
+function uuidv4()
+{
+  $data = random_bytes(16);
+
+  $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+  $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+  return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
