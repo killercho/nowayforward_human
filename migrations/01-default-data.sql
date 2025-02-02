d6dcab722a4e4f3472d3e4128bd6f4a6d5661a31
Syndamia <kamen@syndamia.com>
2025-02-02 14:23:39 +0200

    feat: Add and show proper admin user

--- migrations/01-default-data.sql
+++ migrations/01-default-data.sql
@@ -1,4 +1,8 @@
 USE nwfh;
 
 INSERT IGNORE INTO Users (Username, Password, Role)
-VALUES ("Anon", "", "User");
+VALUES
+    -- Anon cannot be logged into
+    ("Anon", "", "User"),
+    -- Admin password is "Admin"
+    ("Admin", "$2y$10$uAlsPtyuQm/xrKIKGTnnl.VSEKcq6hpKk2ndJGYF3x1YHvTLHY.ai", "Admin");
