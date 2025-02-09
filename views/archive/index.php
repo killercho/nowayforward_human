aac3a0fa5072525edb4e271a61047624781567e4
Syndamia <kamen@syndamia.com>
2025-02-09 20:37:11 +0200

    feat(views/archive): Better layout

--- views/archive/index.php
+++ views/archive/index.php
@@ -2,6 +2,7 @@
     $exists = null;
     $page = null;
     $user = null;
+    $archives = null;
 
     try {
         list($exists, $url) = doesWebsiteExist($url);
@@ -9,25 +10,41 @@
 
         $page = Database\Webpage::fromDB($url);
         $user = Database\Cookie::fromDB($TOKEN);
+        $archives = $page->allArchives();
     }
     catch(Exception $e) {
     }
 ?>
 
 <?php if ($page !== null): ?>
-    <iframe src="<?= "/archives/{$page->WID}/index.php" ?>" scrolling="no" sandbox></iframe>
+    <h1 id="page-header"><?= $page->URL ?></h1>
 
-    <p></p>
-    <form action="/archive/create" method="POST">
-        <input type="hidden" name="url" value="<?= $url ?>">
-        <input type="submit" value="Archive Now!">
-    </form>
-    <!-- Button to add to list -->
-    <!-- Button to delete -->
+    <p id="page-info">
+        Archived <b><?= count($archives) ?></b> times,
+        between
+        <b><?php echo end($archives)->Date; reset($archives); ?></b>
+        and
+        <b><?= current($archives)->Date ?></b>
+    </p>
+
+    <section id="page-buttons">
+        <span class="flex-expand"></span>
+        <form action="/archive/create" method="POST">
+            <input type="hidden" name="url" value="<?= $url ?>">
+            <input type="submit" value="Archive Now!" class="standalone-button">
+        </form>
+        <form action="/list/add" method="GET">
+            <input type="hidden" name="wid" value="<?= current($archives)->WID ?>">
+            <input type="submit" value="Add to a list!" class="standalone-button">
+        </form>
+        <span class="flex-expand"></span>
+    </section>
+
+    <iframe src="<?= "/archives/{$page->WID}/index.php" ?>" scrolling="no" sandbox></iframe>
 
     <h2>Archives by date:</h2>
     <?php
-        foreach ($page->allArchives() as $page) {
+        foreach ($archives as $page) {
             include __DIR__ . '/item.php';
         }
         include_once __DIR__ . '/item_show.php';
